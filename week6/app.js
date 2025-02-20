c0155e19a1b001aafbf16b41ee6032b2be54e944
saraxixi <saraxi.xixi@gmail.com>
2025-02-20 11:59:22 -0800

    add: restructure app to use Jade template engine and implement routing for index and users

--- week6/app.js
+++ week6/app.js
@@ -1,39 +1,41 @@
-// const fs = require('fs')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// fs.writeFile('data.txt', 'This is a message for you!', (err) => {
-//   if (err) {
-//     console.error('Write failed')
-//   } else {
-//     console.log('File written successfully');
-//     fs.readFile('data.txt', 'utf-8', (err, data) => {
-//       if (err) {
-//         console.error(err)
-//       } else {
-//         console.log('File content:', data)
-//       }
-//     });
-//   }
-// });
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const logger = require('./logger.js');
-logger.log();
+var app = express();
 
-const express = require('express');
-const app = express();
-const port = 3000;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-const tasksRouter = require('./routes/tasks.js');
-app.use('/tasks', tasksRouter);
-
-app.get('/', (req, res) => {
-  res.send('Hello and welcome to my site!');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(port, function() { 
-  console.log(`Example app listening on port ${port}!`); 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
