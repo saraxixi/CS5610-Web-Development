75d9bd756fdbe5915c012f6a22e28232f7acc2ce
saraxixi <saraxi.xixi@gmail.com>
2025-02-27 18:07:36 -0800

    feat: implement getTaskById function and format tasks in getAllTasks; add error handling and new error view

--- week6/db.js
+++ week6/db.js
@@ -1,5 +1,4 @@
 const {MongoClient} = require('mongodb');
-const { get } = require('./routes');
 require('dotenv').config();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
@@ -21,17 +20,36 @@ module.exports={
     }
   },
 
-  getAllTasks: async function() {
+  getAllTasks: async function () {
     try {
-      const result = await client
-        .db('cs5610')
-        .collection('tasks')
-        .find()
-        .toArray();
-      return result;
+      const result = await client.db('cs5610').collection('tasks').find().toArray();
+      
+      const formattedTasks = result.map(task => {
+        const keys = Object.keys(task).filter(key => key !== "_id");
+        return keys.map(key => ({
+          _id: task._id,
+          title: key,
+          completed: false
+        }));
+      }).flat();
+      return formattedTasks;
     } catch (err) {
       console.error("get all tasks", err);
       return [];
+    }
+  },
+
+  getTaskById: async function (taskId) {
+    try {
+      const task = await client
+        .db('cs5610')
+        .collection('tasks')
+        .findOne({ _id: new ObjectId(taskId) });
+  
+      return task;
+    } catch (err) {
+      console.error("Error fetching task:", err);
+      return null;
     }
   }
 }
