068791aab0071a2cd2c8d4430e8c70e2391d87d1
saraxixi <saraxi.xixi@gmail.com>
2025-03-13 11:42:33 -0700

    feat: convert TaskList to use state for dynamic task management

--- week9/vite-project/src/components/TaskList.jsx
+++ week9/vite-project/src/components/TaskList.jsx
@@ -1,31 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const tasks = [
-  {
-    id: 1,
-    title: "Review week 9 material",
-    date: "June 4th at 1 pm",
-  },
-  {
-    id: 2,
-    title: "Do quiz 9",
-    date: "June 4th at 6 pm",
-  },
-  {
-    id: 3,
-    title: "Work on assignment 2",
-    date: "June 5th at 8 am",
-  },
-];
+function TasksList() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            title: "Review week 9 material",
+            date: "June 4th at 1 pm",
+        },
+        {
+            id: 2,
+            title: "Do quiz 9",
+            date: "June 4th at 6 pm",
+        },
+        {
+            id: 3,
+            title: "Work on assignment 2",
+            date: "June 5th at 8 am",
+        },
+    ]);
 
-function TaskList() {
-  return (
-    <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>{task.title}</li>
-      ))}
-    </ul>
-  );
+    return (
+        <ul>
+            {tasks.map(task => (
+                <li key={task.id}>{task.title} - {task.date}</li>
+            ))}
+        </ul>
+    );
 }
 
-export default TaskList;
+export default TasksList;
