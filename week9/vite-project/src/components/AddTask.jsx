c0a654e910a476679f89c98c51cb6ed460bd9f1f
saraxixi <saraxi.xixi@gmail.com>
2025-03-13 12:12:37 -0700

    feat: implement controlled inputs for title and date in AddTask component

--- week9/vite-project/src/components/AddTask.jsx
+++ week9/vite-project/src/components/AddTask.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddTask() {
+  const [title, setTitle] = useState('')
+  const [date, setDate] = useState('')
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  };
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value)
+  };
+
   return (
     <form>
       <div className="form-control">
         <label>Title</label>
-        <input type="text" />
+        <input type="text" value={title} onChange={handleTitleChange} />
       </div>
       <div className="form-control">
         <label>Date</label>
-        <input type="text"></input>
+        <input type="text" value={date} onChange={handleDateChange} />
       </div>
         <button type="submit"> Save </button>
     </form>
