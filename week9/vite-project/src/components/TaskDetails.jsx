98bf94042f6f4010023c4957c5afdac34414dc72
saraxixi <saraxi.xixi@gmail.com>
2025-03-27 12:27:13 -0700

    feat: add user fetching in TaskDetails component and update db.json with user data

--- week9/vite-project/src/components/TaskDetails.jsx
+++ week9/vite-project/src/components/TaskDetails.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 function TaskDetails() {
   const { taskId } = useParams();
-  return <div>TaskDetails of task {taskId}</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await fetch(`http://localhost:5000/users?task=${taskId}`);
+        if (response.ok) {
+          const data = await response.json();
+          setUsers(data);
+        } else {
+        console.error("Failed to fetch users");
+        }
+      } catch (error) {
+      console.error("fetchUsers error:", error);
+      }
+    }
+    fetchUsers();
+  }, [taskId]);
+
+  return (
+    <div>
+      <h3>Task Details of task {taskId}</h3>
+      {users.length > 0 ? (
+        users.map((user) => (
+          <p key={user.id}>
+            {user.name} is responsible for this task
+          </p>
+        ))
+      ) : (
+        <p>No users found</p>
+      )}
+    </div>
+  );
 }
 
 export default TaskDetails;
