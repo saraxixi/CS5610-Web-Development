4e785743a59d35d6da0fc603cc0842cb1aff6a33
saraxixi <saraxi.xixi@gmail.com>
2025-04-03 12:10:02 -0700

    feat: enhance LogoutButton to include logout functionality with redirect

--- week9/vite-project/src/components/LogoutButton.jsx
+++ week9/vite-project/src/components/LogoutButton.jsx
@@ -2,7 +2,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 export default function LogoutButton() {
-  useAuth0();
+  const { logout } = useAuth0();
 
-  return <button>Logout</button>
+  return (
+    <button onClick={() => logout({logoutParams: {returnTo: window.location.origin}})}>
+      Logout
+    </button>
+  )
 }
