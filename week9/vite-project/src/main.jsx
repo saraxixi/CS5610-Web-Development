eab79a529c79d3cfebb30493a98b36f2692f7ee4
saraxixi <saraxi.xixi@gmail.com>
2025-04-03 11:39:35 -0700

    feat: integrate Auth0 for authentication and add environment variables

--- week9/vite-project/src/main.jsx
+++ week9/vite-project/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { Auth0ProviderWithHistory } from './components/Auth0ProviderWithHistory.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <Auth0ProviderWithHistory>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </Auth0ProviderWithHistory>
   </BrowserRouter>
 )
