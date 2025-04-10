da9ed2a9af8b9f1a65286facbf30033280984f82
saraxixi <saraxi.xixi@gmail.com>
2025-04-10 14:23:36 -0700

    fix: ensure AddTask component always renders when showForm is true

--- week9/vite-project/src/App.jsx
+++ week9/vite-project/src/App.jsx
@@ -69,7 +69,7 @@ export default function App() {
                 showForm={showForm}
                 onAddTask={toggleShowForm}
               />
-              {showForm && <AddTask />}
+              <AddTask />
             </>
           }
         />
